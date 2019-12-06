6ed12532cf0f74d3bde6358885061ec45d373274
jtclark52 <jtclark52@my.waketech.edu>
2019-12-05 22:13:58 -0500

    Added tests

--- test/features/wtcc.feature
+++ test/features/wtcc.feature
@@ -3,12 +3,13 @@ Feature: Web UI testing using https://en.wikipedia.org.
   I want to be able to search using the Wikipedia website
   And be able to search for “Wake Technical Community College”
 
-  Scenario: Basic UI serach using the Wikipedia website
+  Background:
     Given the url https://en.wikipedia.org
+
+  Scenario: Basic UI serach using the Wikipedia website
     Then I should be able to navigate to the Wikipedia Home Page
     And The Wikipedia Home page should load
 
-  Scenario: Basic UI serach using the Wikipedia website
-    Given the url https://en.wikipedia.org
+  Scenario: Search “Wake Technical Community College”
     Then I should be able to search “Wake Technical Community College”
     And be able to verify that Wake Tech’s content page has a title of “Wake Technical Community College”
--- test/step_defs/test_wtcc.py
+++ test/step_defs/test_wtcc.py
@@ -1,15 +1,23 @@
+import pytest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
-from pytest_bdd import scenario, given, when, then
+from pytest_bdd import scenario, given, when, then, parsers
+
 
 driver = webdriver.Chrome("C:\Python Projects\Project 1\chromedriver.exe")
 
-def test_wikipedia():
+@given('The Wikipedia Home Page is displayed')
+def wikipedia_home():
     driver.get("https://en.wikipedia.org")
+
+@then('find search field')
+def find_input():
     search = driver.find_element_by_id("searchInput")
     search.clear()
 
+@then('Search "wake tech"')
+def search():
     search.send_keys("wake tech")
     search.send_keys(Keys.RETURN)
     time.sleep(5)
@@ -18,4 +26,4 @@ def test_wikipedia():
 
     # print(title)
     assert title == "Wake Technical Community College - Wikipedia"
-    driver.close()
+    driver.quit()
